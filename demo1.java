79239cd0267c0057b46f6210e8492b543703b4f2
Pranavi030 <245123749030@mvsrec.edu.in>
2026-08-17 13:32:36 +0530

    Update demo1.java

--- demo1.java
+++ demo1.java
@@ -2,5 +2,6 @@ public class demo1 {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("Welcome to DevOps Lab");
+        System.out.println("New change made");
     }
 }
